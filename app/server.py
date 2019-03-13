d62dd12995721bfc693e0687938e0cdb998a61c6
Reshma  Ramachandran <r.reshma67@gmail.com>
2019-03-13 11:35:39 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,10 +12,10 @@ from fastai.vision import *
 #export_file_url = 'https://drive.google.com/uc?export=download&id=1mfIfa9qZUbW39XyYSh-Y-EySAD1EuqVz'
 #export_file_name = 'resnet50.pkl'
 
-#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 #export_file_name = 'export.pkl'
 
-export_file_url = 'https://www.dropbox.com/s/jtj8ujrnl01y77h/resnet50.pkl?dl=0'
+#export_file_url = 'https://www.dropbox.com/s/jtj8ujrnl01y77h/resnet50.pkl?dl=0'
 export_file_name = 'resnet50.pkl'
 
 #classes = ['black', 'grizzly', 'teddys']
