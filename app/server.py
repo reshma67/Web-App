6aeb6d86bfbadad77b4ca337fc5c4098f4096cb3
Reshma  Ramachandran <r.reshma67@gmail.com>
2019-03-12 21:06:54 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=1mfIfa9qZUbW39XyYSh-Y-EySAD1EuqVz'
+export_file_url = 'https://drive.google.com/open?id=1mfIfa9qZUbW39XyYSh-Y-EySAD1EuqVz'
 export_file_name = 'resnet50.pkl'
 
 classes = ['tench',
